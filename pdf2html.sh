5f45dd5ba2151daead460c4f4bebad958007630b
nsz <nsz@port70.net>
2011-07-24 14:04:39 +0200

    add new c1x standard draft (n1570), +pdf2html.sh script

---
+++ pdf2html.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+name=$(basename $1 |sed 's/.pdf$//')
+[ $name.pdf = $1 ] || {
+	echo 'usage: ./pdf2html.sh nxxxx.pdf' 1>&2
+	exit 1
+}
+
+pdftotext -layout $name.pdf
+mv $name.txt $name.txt.utf8
+./translit.sh <$name.txt.utf8 >$name.txt
+./tohtml.sh <$name.txt >$name.html
